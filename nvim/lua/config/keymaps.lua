819ecf89685e9d7e6996bb8a660a5b082e0dd85d
mx <mx1545814198@gmail.com>
2023-11-22 17:56:23 +0800

    First commit

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+
+-- DO NOT USE THIS IN YOU OWN CONFIG!!
+-- use `vim.keymap.set` instead
+local map = Util.safe_keymap_set
+
+map({ "i", "n" }, "<A-o>", "<esc>", { desc = "Esc" })
+map("n", "<A-o>", ":noh<enter>", { desc = "Nohelight" })
