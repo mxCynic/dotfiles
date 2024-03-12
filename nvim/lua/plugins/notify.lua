5f98f2682393f31d611ac9431b959b802b2bec62
GitExample <git@example.com>
2024-03-12 13:46:03 +0800

    fixed: change the position of notify

---
+++ nvim/lua/plugins/notify.lua
@@ -0,0 +1,30 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
+
+  require("noice").setup({
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "45%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+    },
+  }),
+}
