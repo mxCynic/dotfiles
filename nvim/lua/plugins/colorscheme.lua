38ee195d38c7b82cf901b862d72960110c309c23
mxCynic <mx1545814198@gmail.com>
2024-05-12 03:29:53 +0800

    change: change the color of dashboard logo

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,7 +12,18 @@ return {
   { "ellisonleao/gruvbox.nvim" },
 
   -- add kastushika
-  { "rebelot/kanagawa.nvim" },
+  {
+    "rebelot/kanagawa.nvim",
+    config = {
+      colors = {
+        palette = {
+          -- change all usages of these colors
+          waveRed = "#30c5bb",
+          autumnRed = "#39c5bb",
+        },
+      },
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
