800fbbef2c2b6d2a66fd197b245611a76f352cce
Lucky Pony <pony@arch>
2024-12-23 17:38:44 +0800

    2024-12-23-17-38 update

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
+    dashboard = { enabled = false },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
