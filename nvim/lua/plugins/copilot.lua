3c340a0d0496dcacb6d17ef9054412f81e191403
mxCynicc <xuqi.wang@stepai.cn>
2025-12-12 14:46:35 +0800

    delete(nvim): 删除copilot

--- nvim/lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "zbirenbaum/copilot.lua",
-  dependencies = {
-    "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
-  },
-  config = function()
-    require("copilot").setup({})
-  end,
-}
