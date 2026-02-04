3892179bcd5d2fe5672e8b3807369993bd618756
xuqiwang <xuqi.wang@stepai.cn>
2026-02-04 17:24:47 +0800

    update

--- zsh/config/alias.zsh
+++ zsh/config/alias.zsh
@@ -8,14 +8,10 @@
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 alias vim=nvim
 alias bdwp="~/Mxbin/baidudisk/opt/baidunetdisk/baidunetdisk"
-alias matlab="~/matlab/R2022b/bin/matlab -nosplash -nodesktop"
 alias chkit="GLFW_IM_MODULE=ibus kitty"
 alias kicat="kitty +kitten icat"
-alias hl="Hyprland"
-alias read="zathura"
 alias battery="upower -i `upower -e | grep 'BAT'`"
 alias mf="musicfox"
-alias ct="countdown"
 alias mv="mv -i"
 alias lg="lazygit"
 alias svim="sudo -E nvim"
@@ -27,4 +23,5 @@ alias kde_x11="/usr/lib/plasma-dbus-run-session-if-needed /usr/bin/startplasma-x
 alias zxvpn="sudo easytier-core -d --no-listener -p udp://211.143.247.66 --network-name stepai --network-secret eztier@stepai0127"
 alias tarui_way_env="WEBKIT_DISABLE_DMABUF_RENDERER=1"
 alias lj="lazyjj"
+alias cat="bat -p"
 
