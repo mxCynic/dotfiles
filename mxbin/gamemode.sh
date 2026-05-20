f7bda48e05cfa0b902b44462d45cd00072cc45ca
xuqiwang <xuqi.wang@stepai.cn>
2026-05-20 09:47:24 +0800

    hypr: update gamemode for Lua config

--- mxbin/gamemode.sh
+++ mxbin/gamemode.sh
@@ -1,22 +1,35 @@
 #!/usr/bin/env sh
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-if [ "$HYPRGAMEMODE" = 1 ]; then
-  hyprctl --batch "\
-        keyword animations:enabled 0;\
-        keyword decoration:drop_shadow 0;\
-        keyword decoration:blur:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0;\
-        keyword unbind  , F1;\
-        keyword unbind  , F2;\
-        keyword unbind  , F3;\
-        keyword unbind  , F4;\
-        keyword unbind  , F5;\
-        keyword unbind  Alt, w;\
-        keyword unbind  CTRL_ALT, A;\
-        "
+if [ "$HYPRGAMEMODE" = true ]; then
+  hyprctl eval '
+    hl.config({
+      animations = {
+        enabled = false,
+      },
+      decoration = {
+        rounding = 0,
+        shadow = {
+          enabled = false,
+        },
+        blur = {
+          enabled = false,
+        },
+      },
+      general = {
+        gaps_in = 0,
+        gaps_out = 0,
+        border_size = 1,
+      },
+    })
+
+    hl.unbind("F1")
+    hl.unbind("F2")
+    hl.unbind("F3")
+    hl.unbind("F4")
+    hl.unbind("F5")
+    hl.unbind("ALT + W")
+    hl.unbind("CTRL + ALT + A")
+  '
   exit
 fi
-hyprctl reload
+hyprctl reload config-only
