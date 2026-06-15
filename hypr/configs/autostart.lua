6bc68ec3caf4d0af2d12c705d8f549dc2b5e4e4e
xuqiwang <xuqi.wang@stepai.cn>
2026-06-16 00:08:54 +0800

    chore(hypr): disable clipboard sync autostart

--- hypr/configs/autostart.lua
+++ hypr/configs/autostart.lua
@@ -11,7 +11,7 @@ hl.on("hyprland.start", function()
   hl.exec_cmd("xrdb -merge ~/.Xresources")
   hl.exec_cmd("nwg-look -a")
   hl.exec_cmd("/usr/lib/mate-polkit/polkit-mate-authentication-agent-1")
-  hl.exec_cmd("sleep 2 && $HOME/.mxbin/clipboard.sh")
+  --hl.exec_cmd("sleep 2 && $HOME/.mxbin/clipboard.sh")
   hl.exec_cmd("hyprsunset")
   hl.exec_cmd("hyprlauncher -d")
   -- 先给mako.service加入这些环境变量
