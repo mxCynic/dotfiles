954fe222bd7314fddb01bcc4d117c4416fcf6f1a
mxcynic <mx1545814198@gmail.com>
2026-08-31 09:23:13 +0800

    feat(hypr): shrink special workspace windows with larger gaps

--- hypr/configs/workspace_rule.lua
+++ hypr/configs/workspace_rule.lua
@@ -1,2 +1,3 @@
 hl.workspace_rule({ workspace = "10", monitor = "eDP-1", default = true })
 hl.workspace_rule({ workspace = "8", persistent = true })
+hl.workspace_rule({ workspace = "special:magic", gaps_out = 160, gaps_in = 20 })
